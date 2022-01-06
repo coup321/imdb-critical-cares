c6ff2a551965f17f4e0233c2a68022415b2b8167
coup321 <coup321@gmail.com>
2022-01-05 21:42:25 -0500

    update main

--- src/load_data.py
+++ src/load_data.py
@@ -1,17 +1,17 @@
 from os import path
 import tensorflow_datasets as tfds
 import tensorflow as tf
-
+from pathlib import Path
 def load_data(batch_size: int, train_size: float, data_dir: path) -> tuple:
 
-    split = [f'train[:{train_size*100:.0f}%]', f'train[{train_size*100:.0f}%]:', f'test']                          
+    split = [f'train[:{train_size*100:.0f}%]', f'train[{train_size*100:.0f}%:]', f'test']                          
 
     train, val, test = tfds.load(name='imdb_reviews',
                                  data_dir=data_dir,
                                  split=split,
                                  with_info=False, 
                                  batch_size=batch_size, 
-                                 try_gcs=True) 
-
+                                 try_gcs=True,
+                                 as_supervised=True) 
+    
     return train, val, test
-
