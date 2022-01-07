a8fc6222b656a88ee86e63ea2384b5eb3bdd635a
coup321 <coup321@gmail.com>
2022-01-07 17:12:56 -0500

    update for colab

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ import tensorflow as tf
 tf.get_logger().setLevel('ERROR') 
 import os
 
-
-#gpus = tf.config.list_physical_devices('GPU')
-#tf.config.experimental.set_memory_growth(gpus[0], True)
-#tf.config.set_logical_device_configuration(gpus[0], tf.config.LogicalDeviceConfiguration(memory_limit=2048))
-
-#os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-
-
 EPOCHS = 2
 LEARNING_RATE = 5e-5
 BATCH_SIZE = 32
@@ -26,6 +18,10 @@ PREPROCESSER = 'BERT_LR_Preprocesser'
 MODEL = 'BERT_LR_Classifier'
 USE_TPU = False
 
+tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+tfhub_handle_encoder = 'https://tfhub.dev/google/experts/bert/pubmed/2'
+
+
 preprocesser_dict = {
     'BERT_LR_Preprocesser' : BERT_LR_Preprocesser([], 128, tfhub_handle_preprocess)
 }
@@ -33,9 +29,6 @@ preprocesser_dict = {
 models = {
     'BERT_LR_Classifier' : BERT_LR_Classifier(tfhub_handle_encoder)
 }
-
-#tpu=False, val=True, test=False
-
 def main():
     #load data
     train, val, test = load_data(batch_size=BATCH_SIZE,train_size=TRAIN_SIZE, data_dir=DATA_DIR)
