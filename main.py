1334959b1bf700a7133f3160306537dea2ec1390
coup321 <coup321@gmail.com>
2022-01-14 17:22:48 -0500

    for colab

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
 
     #save model
     with tf.device('/job:localhost'):
-        model.save(os.path.join('./saved_models/BERT_LR_Classifier', datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
+        model.save(os.path.join('gs://cjc-tensorboard-logs', datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
 
 if __name__ =="__main__":
     main()
