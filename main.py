c0659937461a3fc5de5d6f80193a9d6e6924226a
coup321 <coup321@gmail.com>
2022-01-14 17:05:02 -0500

    fix syntax error

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
         model = train_model(processed_train, processed_val, models[MODEL], LEARNING_RATE, EPOCHS, callbacks=LOG)
 
     #save model
-    model.save(os.path.join('./saved_models/BERT_LR_Classifier', datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    model.save(os.path.join('./saved_models/BERT_LR_Classifier', datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
 
 if __name__ =="__main__":
     main()
